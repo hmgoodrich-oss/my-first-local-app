dbea830677a139fec9cd342793f48857b44e2918
Hannah Goodrich <hmgoodrich@seu.edu>
2026-04-12 19:32:47 -0400

    initial setup

---
+++ .venv/game.py
@@ -0,0 +1,16 @@
+import random
+
+class Game: 
+    def __init__(self):
+        self.suspects = ["Miss Scarlet", "Professor Plum", "Mrs. Peacock", "Reverend Green", "Colonel Mustard", "Mrs. White"]
+        self.weapons = ["Candlestick", "Dagger", "Lead Pipe", "Revolver", "Rope", "Wrench"]
+        self.rooms = ["Kitchen", "Ballroom", "Conservatory", "Dining Room", "Billiard Room", "Library", "Lounge", "Hall", "Study"]
+        
+        self.solution = {}
+
+        def setup_game(self):
+            self.solution = {
+                "suspect": random.choice(self.suspects),
+                "weapon": random.choice(self.weapons),
+                "room": random.choice(self.rooms)   
+            }
