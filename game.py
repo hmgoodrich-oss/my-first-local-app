91640e4f102df1023b92fd5a1217e794657a0cf1
Hannah Goodrich <hmgoodrich@seu.edu>
2026-04-27 21:29:07 -0400

    initial gp: user can enter guess

--- game.py
+++ game.py
@@ -3,42 +3,47 @@ import random
 import time
 
 class Game: 
-    def __init__(self):
-        self.suspects = ["Miss Scarlet", "Professor Plum", "Mrs. Peacock", "Mr. Green", "Colonel Mustard", "Mrs. White"]
-        self.weapons = ["Candlestick", "Dagger", "Lead Pipe", "Revolver", "Rope", "Wrench"]
-        self.rooms = ["Kitchen", "Ballroom", "Conservatory", "Dining Room", "Billiard Room", "Library", "Lounge", "Hall", "Study"]
-        
-        self.solution = {}
-        self.setup_game()
+     def __init__(self):
+          self.suspects = ["Miss Scarlet", "Professor Plum", "Mrs. Peacock", "Mr. Green", "Colonel Mustard", "Mrs. White"]
+          self.weapons = ["Candlestick", "Dagger", "Lead Pipe", "Revolver", "Rope", "Wrench"]
+          self.rooms = ["Kitchen", "Ballroom", "Conservatory", "Dining Room", "Billiard Room", "Library", "Lounge", "Hall", "Study"]
+          
+          self.solution = {}
+          self.setup_game()
 
-    def setup_game(self):
-            self.solution = {
-                "suspect": random.choice(self.suspects),
-                "weapon": random.choice(self.weapons),
-                "room": random.choice(self.rooms)   
-            }
+     def setup_game(self):
+          self.solution = {
+               "suspect": random.choice(self.suspects),
+               "weapon": random.choice(self.weapons),
+               "room": random.choice(self.rooms)   
+          }
     
-    def check_accusation(self, suspect, weapon, room):
-         incorrect = []
-         if suspect != self.solution["suspect"]:
-              incorrect.append("suspect", suspect)
-              print()
-         if weapon != self.solution["weapon"]:
-              incorrect.append("weapon", weapon)
-         if room != self.solution["room"]:
-              incorrect.append("room", room)
+     def check_accusation(self, args):
+          if len(args) < 3:
+              print("Accusation should be in the form 'suspect, weapon, room'")
+          suspect = args[0]
+          weapon = args[1]
+          room = args[2]
+          incorrect = []
+          if suspect != self.solution["suspect"]:
+               incorrect.append(f"suspect: {suspect}")
+               print()
+          if weapon != self.solution["weapon"]:
+               incorrect.append(f"weapon: {weapon}")
+          if room != self.solution["room"]:
+               incorrect.append(f"room: {room}")
      #give a clue? 
-         if incorrect != []:
-          print(f"You realize {random.choice(incorrect)} couldn't be it..by jove, it's all coming together!")
-         else:
-          print("You're on the right track!")
-     
-    def make_accusation(self, suspect, weapon, room):
-         if suspect == self.solution["suspect"] and weapon == self.solution["weapon"] and room == self.solution["room"]:
+          if incorrect != []:
+               print(f"You realize {random.choice(incorrect)} couldn't be it..by jove, it's all coming together!")
+          else:
+               print("You're on the right track!")
+
+     def make_accusation(self, suspect, weapon, room):
+          if suspect == self.solution["suspect"] and weapon == self.solution["weapon"] and room == self.solution["room"]:
                print(f"You've done it! Arrest {self.solution['suspect']}")
                # break
-         else:
-              print(f"You've got the wrong man...it was {self.solution['suspect']} with the {self.solution['weapon']} in the {self.solution['room']}")
+          else:
+               print(f"You've got the wrong man...it was {self.solution['suspect']} with the {self.solution['weapon']} in the {self.solution['room']}")
               
  # timer - googled how to do a countdown in python
 # start_time = time.time()
@@ -48,6 +53,7 @@ class Game:
 #      print("TIME'S UP! YOU LOSE.")
 #      break
      
+game = Game()
 # gameplay
 tries = 0
 print("It's a dark and stormy night...and your dinner party has just been interrupted by a murder! It's up to you to find out who, how, and where.")
@@ -55,4 +61,8 @@ while tries <= 10:
      print("1. Make an accusation")
      print("2. Make an arrest")
      choice = int(input("Let's:"))
-     if choice = 1: 
+     if choice == 1:
+          ans = input("Who did it? With what? and Where?")
+          args = ans.split(',')
+          print(f'args: {args}')
+          game.check_accusation(args)
